b542b72ae7ba6ddf4a96c4f65689b37d72bca082
JollyChua <74226122+JollyChua@users.noreply.github.com>
2022-01-18 03:05:07 +0800

    LogIn and SignUp's CSS is good

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,36 +1,81 @@
 <template>
   <div id="forAdvertisement">
+    <!---firstform--->
     <form id="LoginForm">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-        <div id="emailHelp" class="form-text">
-          We'll never share your email with anyone else.
+      <div v-if="display === 'LogInForm'">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label"
+            >Email address</label
+          >
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" class="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <!--password-->
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1"
+            >Check me out</label
+          >
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <br />
+        <hr />
+        <button
+          type="submit"
+          v-on:click="gotoSignUp"
+          class="btn btn-outline-primary"
+        >
+          Create New Account
+        </button>
+      </div>
+      <!---new form--->
+      <div v-if="display === 'SignUpForm'">
+        <div class="mb-3">
+          <label for="validationCustom01" class="form-label">User Name</label>
+          <input type="text" class="form-control" id="validationCustom01" />
+          <div class="valid-feedback">Looks good!</div>
+        </div>
+        <div class="mb-3">
+          <label for="inputEmail4" class="form-label">Email</label>
+          <input type="email" class="form-control" id="inputEmail4" />
+          <div class="valid-feedback">Looks good!</div>
+        </div>
+        <div class="col-md-6">
+          <label for="inputPassword4" class="form-label">Password</label>
+          <input type="password" class="form-control" id="inputPassword4" />
+        </div>
+        <div class="col-md-6">
+          <label for="inputPassword4" class="form-label">Repeat Password</label>
+          <input type="password" class="form-control" id="inputPassword4" />
+        </div>
+        <div class="col-12">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck" />
+            <label class="form-check-label" for="gridCheck">
+              Agree to terms and condition
+            </label>
+          </div>
+        </div>
+        <hr />
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary">Sign up</button>
         </div>
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-      <br />
-      <hr />
-      <button type="submit" class="btn btn-outline-primary">
-        Create New Account
-      </button>
     </form>
   </div>
 </template>
@@ -39,6 +84,16 @@
 <script>
 export default {
   name: "Login",
+  data: function () {
+    return {
+      display: "LogInForm",
+    };
+  },
+  methods: {
+    gotoSignUp: function () {
+      this.display = "SignUpForm";
+    },
+  },
 };
 </script>
 
@@ -60,9 +115,9 @@ export default {
   border: 2px solid yellow;
   border-radius: 5px;
   width: 600px;
-  height: 460px;
-  padding: 50px 50px 80px 50px;
-  margin-left: 750px;
+  height: 470px;
+  padding: 40px 50px 80px 50px;
+  margin-left: 780px;
   margin-top: 50px;
 }
 
