4e8311f7698204772517b322f65d763db2774048
JollyChua <74226122+JollyChua@users.noreply.github.com>
2022-01-18 05:07:54 +0800

    created a card for classes

---
+++ src/components/ClassCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+
+<script>
+export default {
+  name: "ClassCard",
+};
+</script>
+
+
+<style scoped>
+</style>
--- src/components/DanceClasses.vue
+++ src/components/DanceClasses.vue
@@ -1,10 +1,26 @@
- <template>
+  <template>
   <div>
     <h1>All Classes</h1>
     <button @click="showJoke">Show Joke</button>
     <button @click="classList">Classes</button>
-    <div>
-      <p>{{ classes }}</p>
+    <div
+      class="card"
+      style="width: 18rem"
+      v-for="id in classes"
+      v-bind:key="id"
+    >
+      <!--img src="..." class="card-img-top" alt="..."-->
+      <div class="card-body">
+        <h5 class="card-title">{{ id.name }}</h5>
+        <p class="card-text">
+          Location: {{ id.location }}
+          <br />
+          Schedule: {{ id.schedule }}
+          <br />
+          Price per session: {{ id.price }}
+        </p>
+        <a href="#" class="btn btn-primary">Enroll</a>
+      </div>
     </div>
     <div>
       <p>{{ joke }}</p>
