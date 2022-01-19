8a92adc448a91ed101b6914ff59d2eaa7d600361
JollyChua <74226122+JollyChua@users.noreply.github.com>
2022-01-20 02:40:38 +0800

    Home.vue has nav section, WIP

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,119 @@
 <template>
   <div>
-    <button @click="displayMe">Classes</button>
-    <div v-for="n in Ako" v-bind:key="n.accountNum">
+    <div
+      class="nav flex-column nav-pills"
+      id="v-pills-tab"
+      role="tablist"
+      aria-orientation="vertical"
+    >
+      <a
+        class="nav-link active"
+        id="v-pills-home-tab"
+        data-toggle="pill"
+        href="#v-pills-home"
+        role="tab"
+        aria-controls="v-pills-home"
+        aria-selected="true"
+        >Home</a
+      >
+      <a
+        class="nav-link"
+        id="v-pills-profile-tab"
+        data-toggle="pill"
+        href="#v-pills-profile"
+        role="tab"
+        aria-controls="v-pills-profile"
+        aria-selected="false"
+        >Profile</a
+      >
+      <a
+        class="nav-link"
+        id="v-pills-messages-tab"
+        data-toggle="pill"
+        href="#v-pills-messages"
+        role="tab"
+        aria-controls="v-pills-messages"
+        aria-selected="false"
+        >Classes</a
+      >
+      <a
+        class="nav-link"
+        id="v-pills-messages-tab"
+        data-toggle="pill"
+        href="#v-pills-messages"
+        role="tab"
+        aria-controls="v-pills-messages"
+        aria-selected="false"
+        >Enrolled Classes</a
+      >
+      <a
+        class="nav-link"
+        id="v-pills-messages-tab"
+        data-toggle="pill"
+        href="#v-pills-messages"
+        role="tab"
+        aria-controls="v-pills-messages"
+        aria-selected="false"
+        >Friends</a
+      >
+      <a
+        class="nav-link"
+        id="v-pills-messages-tab"
+        data-toggle="pill"
+        href="#v-pills-messages"
+        role="tab"
+        aria-controls="v-pills-messages"
+        aria-selected="false"
+        >Directions</a
+      >
+      <a
+        class="nav-link"
+        id="v-pills-settings-tab"
+        data-toggle="pill"
+        href="#v-pills-settings"
+        role="tab"
+        aria-controls="v-pills-settings"
+        aria-selected="false"
+        >Settings</a
+      >
+    </div>
+    <div v-for="n in Name" v-bind:key="n.accountNum">
       <h3 v-if="Name">Hello, {{ n.email }}</h3>
       <h3 v-if="!Name">Hello You are not logged in!</h3>
+    </div>
+    <div class="tab-content" id="v-pills-tabContent">
+      <div
+        class="tab-pane fade show active"
+        id="v-pills-home"
+        role="tabpanel"
+        aria-labelledby="v-pills-home-tab"
+      >
+        ...
+      </div>
+      <div
+        class="tab-pane fade"
+        id="v-pills-profile"
+        role="tabpanel"
+        aria-labelledby="v-pills-profile-tab"
+      >
+        ...
+      </div>
+      <div
+        class="tab-pane fade"
+        id="v-pills-messages"
+        role="tabpanel"
+        aria-labelledby="v-pills-messages-tab"
+      >
+        ...
+      </div>
+      <div
+        class="tab-pane fade"
+        id="v-pills-settings"
+        role="tabpanel"
+        aria-labelledby="v-pills-settings-tab"
+      >
+        ...
+      </div>
     </div>
   </div>
 </template>
@@ -19,20 +129,20 @@ export default {
     };
   },
   created: async function () {
-    const response = await axios.get("");
-    console.log(response);
+    const response = await axios.get("http://localhost:3000");
     this.Name = response.data;
-  },
-  methods: {
-    async displayMe() {
-      const response = await axios.get("http://localhost:3000");
-      this.Ako = response.data;
-      console.log(response.data);
-    },
+    console.log(response.data);
   },
 };
 </script>
 
 
 <style scoped>
+#v-pills-tab {
+  background-color: white;
+  width: 400px;
+  height: 900px;
+  left: 0;
+  position: fixed;
+}
 </style>
